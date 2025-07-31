7514b08d1ad1827dc66e13867c452c30bbd4b665
Samuel Xavier <sancrisxa@gmail.com>
2025-07-31 14:28:14 -0300

    first commit

---
+++ src/test/java/br/com/sancrisxa/task_manager_backend/TaskManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sancrisxa.task_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
